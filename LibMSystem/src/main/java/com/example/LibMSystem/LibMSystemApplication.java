87a0b8de2f5d24247e290e70b50dcccbba16bf92
kaloa2025 <aalokchoudhari2021@gmail.com>
2024-07-13 10:17:27 +0530

    initial

---
+++ LibMSystem/src/main/java/com/example/LibMSystem/LibMSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibMSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibMSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibMSystemApplication.class, args);
+	}
+
+}
---
+++ LibMSystem/src/test/java/com/example/LibMSystem/LibMSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibMSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibMSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
